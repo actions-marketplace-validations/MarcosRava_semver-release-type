e98f9c55cee82a82c7852f846938c869d3d291df
Marcos Rava <marcosrava@gmail.com>
2022-03-21 13:09:52 -0300

    feat(initial): Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import semver from 'semver'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const currentVersion: string = core.getInput('current_version')
+    const nextVersion: string = core.getInput('next_version')
+    core.debug(`${currentVersion} -> ${nextVersion}`)
+    const releaseType = semver.diff(currentVersion, nextVersion)
+    core.setOutput('release_type', releaseType)
+    core.setOutput('next_version', nextVersion)
+    core.setOutput('current_version', currentVersion)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
